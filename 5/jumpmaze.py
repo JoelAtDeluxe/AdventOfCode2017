fe8e1f98847b87df9c223b093beb96d7de927e0c
Joel Smith <joel.smith@bydeluxe.com>
2018-12-23 14:11:39 -0800

    day 5 part 2 solution

--- 5/jumpmaze.py
+++ 5/jumpmaze.py
@@ -7,7 +7,7 @@ def load_file(path):
     return lines
 
 
-def navigate_maze(maze):
+def navigate_maze(maze, update_cell_func):
     position = 0
     next_position = 0
     jump_count = 0
@@ -15,10 +15,21 @@ def navigate_maze(maze):
     while 0 <= position < len(maze):
         jump_count += 1
         next_position = position + maze[position]
-        maze[position] += 1
+        maze[position] = update_cell_func(maze[position])
         position = next_position
 
+    # print( maze )
     print(f'Escaped after {jump_count} steps!')
 
+
+def plus_one(value):
+    return value + 1
+
+def part_two_logic(value):
+    return value + 1 if value <= 2 else value - 1
+
+# _maze = [0, 3, 0, 1, -3]
 _maze = load_file('input.txt')
-navigate_maze(_maze)
+
+# navigate_maze(_maze, plus_one)  # p1 solution
+navigate_maze(_maze, part_two_logic)  # p1 solution
