e472f41912fbf4d7e640a931b715efc1c3284a65
Joel Smith <joel.smith@bydeluxe.com>
2018-12-23 10:00:06 -0800

    4 part 2 solution

--- 4/passphrase.py
+++ 4/passphrase.py
@@ -11,10 +11,22 @@ def load_file(path):
 if __name__ == '__main__':
     passphrases = load_file('passwords.txt')
 
-    valid_count = 0
-    for phrase in passphrases:
-        words = phrase.split()
-        if len(words) == len(set(words)):
-            valid_count = valid_count + 1
+    def has_no_repeat_words(words):
+        return len(words) == len(set(words))
 
-    print(f"There are {valid_count} valid passwords")
+    def has_no_anagrams(words):
+        sorted_words = [''.join(sorted(word)) for word in words]
+        return len(sorted_words) == len(set(sorted_words))
+
+    intermediate = map(lambda x: x.split(), passphrases)
+    # intermediate = filter(has_no_repeat_words, intermediate)  # agagram work does the same thing
+    intermediate = filter(has_no_anagrams, intermediate) 
+    valid_passwords = list(intermediate)
+    
+    # P1 solution
+    # for phrase in passphrases:
+    #     words = phrase.split()
+    #     if len(words) == len(set(words)):
+    #         valid_count = valid_count + 1
+
+    print(f"There are {len(valid_passwords)} valid passwords")
