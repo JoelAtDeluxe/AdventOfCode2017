a44e8411c881c1af22243c54c1d42d1907ec973b
Joel Smith <joel.smith@bydeluxe.com>
2019-01-01 21:10:35 -0800

    solution to day 9 part 2

--- 9/junkstream.py
+++ 9/junkstream.py
@@ -4,21 +4,25 @@ def main():
     with open('input.txt') as fh:
         content = fh.read()
 
-    currently_reading = None
+    currently_reading_junk = False
     group_score = 0
     group_nesting = 0
     advance_char = False
+    removed_char_count = 0
+    last_junk_index = 0
 
     for idx, char in enumerate(content):
         if advance_char:
             advance_char = False
             continue
 
-        if currently_reading == 'junk':
+        if currently_reading_junk:
             if char == '!':
+                removed_char_count -= 2  # exclude this character and next
                 advance_char = True
             elif char == '>':
-                currently_reading = None
+                currently_reading_junk = False
+                removed_char_count += idx - last_junk_index - 1
         else:
             if char == '{':
                 group_nesting += 1
@@ -26,11 +30,11 @@ def main():
                 group_score += group_nesting
                 group_nesting -= 1
             elif char == '<':
-                currently_reading = 'junk'
-            else:  # commas, but these don't seem to matter, given the other rules
-                print(f"Found odd character at position: {idx}")
-    
+                currently_reading_junk = True
+                last_junk_index = idx
+            
     print(f'Total group score: {group_score} group nesting (should be 0): {group_nesting}')
+    print(f'Removed non-cancelled garbage characters: {removed_char_count}')
 
 
 if __name__ == "__main__":
