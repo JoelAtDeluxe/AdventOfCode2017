a58d1f6a457aa4650ce47865a14ba3c5a7dc08ee
Joel Smith <joel.smith@bydeluxe.com>
2019-01-01 22:58:00 -0800

    refactoring effort

--- 11/hexed.py
+++ 11/hexed.py
@@ -4,6 +4,61 @@ def read_input():
         return fh.read()
 
 
+def cancel_opposite_directions(steps, dir1, dir2):
+    copy = {**steps}
+    if copy[dir1] >= copy[dir2]:
+        copy[dir1] -= copy[dir2]
+        copy[dir2] = 0
+    else:
+        copy[dir2] -= copy[dir1]
+        copy[dir1] = 0
+    return copy
+
+
+def cancel_short_loops(steps, dir1, dir2, dir3):
+    copy = {**steps}
+    loops = min(copy[dir1], copy[dir2], copy[dir3])
+    copy[dir1] -= loops
+    copy[dir2] -= loops
+    copy[dir3] -= loops
+    return copy
+
+
+def merge_adjacent_directions(steps, left, center, right):
+    # e.g. moving se, then sw is the same as moving s. Call this with ('se', 's', 'sw')
+    copy = {**steps}
+    steps_center = min(steps[left], steps[right])
+    copy[left] -= steps_center
+    copy[right] -= steps_center
+    copy[center] += steps_center
+    return copy
+
+
+def calc_steps(some_steps):
+        # Should be reduced first
+        return sum(some_steps.values())
+
+
+def reduce_steps(steps):
+    # a couple of optimizations
+    # 1: embed the below 3 functions in here, to avoid copies being generated at each step
+    # 2: somehow communicate back the changes to be made, rather than returning the result set
+    # 2: if/else around each operation/set, if you know the last step made
+    copy = {**steps}
+    copy = cancel_short_loops(copy, 'n', 'se', 'sw')
+    copy = cancel_short_loops(copy, 's', 'ne', 'nw')
+    copy = cancel_opposite_directions(copy, 'n', 's')
+    copy = cancel_opposite_directions(copy, 'ne', 'sw')
+    copy = cancel_opposite_directions(copy, 'nw', 'se')
+    copy = merge_adjacent_directions(copy, 'nw', 'n', 'ne')
+    copy = merge_adjacent_directions(copy, 'n', 'ne', 'se')
+    copy = merge_adjacent_directions(copy, 'ne', 'se', 's')
+    copy = merge_adjacent_directions(copy, 'se', 's', 'sw')
+    copy = merge_adjacent_directions(copy, 's', 'sw', 'nw')
+    copy = merge_adjacent_directions(copy, 'sw', 'nw', 'n')
+    return {k: v for k, v in copy.items()}
+
+
 def main():
     actions = read_input().split(',')
     # actions = "se,sw,se,sw,sw".split(',')
@@ -20,55 +75,14 @@ def main():
         's': 0,
         'sw': 0,
     }
-
-    def cancel_opposite_directions(dir1, dir2):
-        if steps[dir1] >= steps[dir2]:
-            steps[dir1] -= steps[dir2]
-            steps[dir2] = 0
-        else:
-            steps[dir2] -= steps[dir1]
-            steps[dir1] = 0
     
-    def cancel_short_loops(dir1, dir2, dir3):
-        loops = min(steps[dir1], steps[dir2], steps[dir3])
-        steps[dir1] -= loops
-        steps[dir2] -= loops
-        steps[dir3] -= loops
-
-    def merge_adjacent_directions(left, center, right):
-        # e.g. moving se, then sw is the same as moving s. Call this with ('se', 's', 'sw')
-        steps_left = steps[left]
-        steps_right = steps[right]
-        steps_center = min(steps_left, steps_right)
-        steps[left] -= steps_center
-        steps[right] -= steps_center
-        steps[center] += steps_center
-
-    def reduce_steps():
-        cancel_short_loops('n', 'se', 'sw')
-        cancel_short_loops('s', 'ne', 'nw')
-        cancel_opposite_directions('n', 's')
-        cancel_opposite_directions('ne', 'sw')
-        cancel_opposite_directions('nw', 'se')
-        merge_adjacent_directions('nw', 'n', 'ne')
-        merge_adjacent_directions('n', 'ne', 'se')
-        merge_adjacent_directions('ne', 'se', 's')
-        merge_adjacent_directions('se', 's', 'sw')
-        merge_adjacent_directions('s', 'sw', 'nw')
-        merge_adjacent_directions('sw', 'nw', 'n')
-        return {k: v for k, v in steps.items()}
-
-    def calc_steps(some_steps):
-        # Should be reduced first
-        return sum(some_steps.values())
-
     wander_distance = 0
     distance_from_home = 0
 
     # Chart the overall steps
     for action in actions:
         steps[action] += 1
-        steps = reduce_steps()
+        steps = reduce_steps(steps)
         distance_from_home = calc_steps(steps)
         if wander_distance < distance_from_home:
             wander_distance = distance_from_home
