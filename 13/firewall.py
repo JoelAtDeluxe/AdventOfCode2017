f0428658d9c3528d493387918fe65b1df4695fe9
Joel Smith <joel.smith@bydeluxe.com>
2019-03-24 20:22:39 -0700

    corrections for part 1 plus some part 2 work

--- 13/firewall.py
+++ 13/firewall.py
@@ -13,32 +13,44 @@ def parse_input():
 
 
 def main():
-    # parsed_scanners = parse_input()
-    parsed_scanners = [(0, 3), (1, 2), (4, 4), (6, 4)]
-    scanners = [Scanner(t) for t in parsed_scanners]
+    parsed_scanners = parse_input()
+    # parsed_scanners = [(0, 3), (1, 2), (4, 4), (6, 4)]
+    scanners = [Scanner(*t) for t in parsed_scanners]
     max_layer = scanners[-1].layer
 
     step = 0
     # part 1
-    sev, stop_layer = calc_severity(scanners, max_layer, step)
+    sev, _ = calc_severity(scanners, max_layer, step)
     print(f'severity on step: {step} is: {sev}')
 
-        
+    # part 2
+    stop_checking = False
+    step = 0
+    while not stop_checking:
+        sev, stopped_early = calc_severity(scanners, max_layer, step)
+        if stopped_early:
+            stop += 1
+        else:
+            stop_checking = True
 
-def calc_severity(scanners, num_layers, step_num, stop_on_nonzero=False):
+    print(f"Found safe path on second: {step}")
+
+
+def calc_severity(scanners, num_layers, step_num, stop_on_collision=False):
     severity = 0
     layer_ptr = 0
-    
+
     for layer_sec in range(num_layers+1):
         next_scanner = scanners[layer_ptr]
         if layer_sec == next_scanner.layer:
             layer_ptr += 1
             if next_scanner.pos_at_step(step_num) == 0:
                 severity += next_scanner.severity
-                        if stop_on_nonzero and severity > 0:
-            return severity, layer_sec
-                
-    return severity, num_layers+1
+                if stop_on_collision:
+                    return severity, True
+
+    return severity, False
+
 
 class Scanner(object):
     def __init__(self, layer, depth):
