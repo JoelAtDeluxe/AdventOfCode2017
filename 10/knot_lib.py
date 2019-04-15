2cfcb5e0dbe8273db51590bfc6981b3420e1a604
Joel Smith <Hikash@gmail.com>
2019-04-15 11:09:07 -0700

    Refactored knot-lib out of the solution, for reuse

---
+++ 10/knot_lib.py
@@ -0,0 +1,73 @@
+from typing import List
+
+class KnotHash(object):
+    def __init__(self, sparse_hash, ops):
+        self.sparse_hash = sparse_hash
+        self.ops = ops
+
+    def knot_hash(self, num_rounds=64):
+        cur_pos = 0
+        skip_size = 0
+
+        for _ in range(num_rounds):
+            cur_pos, skip_size = self._do_round(cur_pos, skip_size)
+
+    def _do_round(self, current_position, skip_size):
+        l_len = len(self.sparse_hash)
+
+        for action in self.ops:
+            if action > 0:
+                start_idx = current_position
+                end_idx = (current_position + action - 1) % l_len
+                self._reverse_circular(start_idx, end_idx)
+
+            current_position = (current_position + action + skip_size) % l_len
+            skip_size += 1
+
+        return current_position, skip_size
+
+    def _reverse_circular(self, start, end):
+        some_list = self.sparse_hash  # ptr to list (in case we want to make this pure)
+
+        list_len = len(some_list)
+        if start > end:
+            true_length = (list_len - start) + end + 1
+        else:
+            true_length = (end - start) + 1
+
+        for i in range(true_length // 2):
+            start_idx = (start + i) % list_len
+            end_idx = end - i
+            end_idx = end_idx if end_idx >= 0 else list_len + end_idx
+
+            some_list[start_idx], some_list[end_idx] = some_list[
+                end_idx], some_list[start_idx]
+
+        return some_list
+
+    @property
+    def check_product(self):
+        return self.sparse_hash[0] * self.sparse_hash[1]
+
+    def dense_hash(self):
+        dh = [0 for _ in range(16)]
+
+        for block_idx in range(16):
+            xord = self.sparse_hash[block_idx * 16]
+            for x_index in range(15):
+                xord = xord ^ self.sparse_hash[block_idx * 16 + 1 + x_index]
+            dh[block_idx] = xord
+
+        return KnotHash.to_hex(dh)
+
+    @staticmethod
+    def to_hex(int_list: List[int]):
+        result = ['' for _ in range(len(int_list)*2)]
+        symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+
+        for i, v in enumerate(int_list):
+            high = v // 16
+            low = v % 16
+            result[2*i], result[(2*i)+1] = symbols[high], symbols[low]
+
+        return ''.join(result)
--- 10/knots.py
+++ 10/knots.py
@@ -1,37 +1,16 @@
-from typing import List
+from knot_lib import KnotHash
 
 
 def main(use_real=True, part_one=False):
-    spase_hash, ops, rounds = load_data(use_real, part_one)
+    sparse_hash, ops, rounds = load_data(use_real, part_one)
 
-    cur_pos = 0
-    skip_size = 0
-
-    for _ in range(rounds):
-        cur_pos, skip_size = knot_hash(spase_hash, ops, cur_pos, skip_size)
-
-    check_product = spase_hash[0] * spase_hash[1]
-    print(f"After {rounds} round(s), CheckProduct for sparse hash is: {check_product}")
+    kh = KnotHash(sparse_hash, ops)
+    kh.knot_hash(rounds)
+    
+    print(f"After {rounds} round(s), CheckProduct for sparse hash is: {kh.check_product}")
 
     if use_real and not(part_one):
-        dense_hash = to_dense_hash(spase_hash)
-        print(f'Dense Hash is: {to_hex(dense_hash)}')
-
-
-def knot_hash(l, ops, current_position, skip_size):
-    l_len = len(l)
-
-    for action in ops:
-        # import pdb; pdb.set_trace()
-        if action > 0:
-            start_idx = current_position
-            end_idx = (current_position + action - 1) % l_len
-            reverse_circular(l, start_idx, end_idx)
-
-        current_position = (current_position + action + skip_size) % l_len
-        skip_size += 1
-
-    return current_position, skip_size
+        print(f'Dense Hash is {kh.dense_hash()}')
 
 
 def load_data(use_real: bool, part_one: bool):
@@ -50,62 +29,10 @@ def load_data(use_real: bool, part_one: bool):
     return l, ops, rounds
 
 
-def to_dense_hash(sparse_hash: List[int]):
-    dense_hash = [0 for _ in range(16)]
-
-    for block_idx in range(16):
-        xord = sparse_hash[block_idx * 16]
-        for x_index in range(15):
-            xord = xord ^ sparse_hash[block_idx * 16 + 1 + x_index]
-        dense_hash[block_idx] = xord
-
-    return dense_hash
-
-
-def to_hex(dense_hash: List[int]):
-    result = ['' for _ in range(len(dense_hash)*2)]
-    symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-
-    for i, v in enumerate(dense_hash):
-        high = v // 16
-        low = v % 16
-        result[2*i], result[(2*i)+1] = symbols[high], symbols[low]
-
-    return ''.join(result)
-
-
-def print_list(l, current_position):
-    str_l = [str(i) for i in l]
-    print(' '.join(str_l[:current_position]), end='')
-    print(f' [{str_l[current_position]}] ', end='')
-    print(' '.join(str_l[current_position + 1:]))
-
-
 def read_input():
     with open('input.txt', 'r') as fh:
         return fh.read()
 
 
-def reverse_circular(l, start, end):
-    # copy = [*l]
-    some_list = l  # ptr to list (in case we want to make this pure)
-
-    list_len = len(some_list)
-    if start > end:
-        true_length = (list_len - start) + end + 1
-    else:
-        true_length = (end - start) + 1
-
-    for i in range(true_length // 2):
-        start_idx = (start + i) % list_len
-        end_idx = end - i
-        end_idx = end_idx if end_idx >= 0 else list_len + end_idx
-
-        some_list[start_idx], some_list[end_idx] = some_list[
-            end_idx], some_list[start_idx]
-
-    return some_list
-
-
 if __name__ == "__main__":
-    main()
+    main(part_one=False)
