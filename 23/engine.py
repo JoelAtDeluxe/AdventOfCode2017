4a9266a028055b89262bf433f7033e6bc26f2146
Joel Smith <Hikash@gmail.com>
2019-06-28 17:11:01 -0700

    efficency upgrade + more actions

--- 23/engine.py
+++ 23/engine.py
@@ -1,3 +1,5 @@
+import math
+
 
 class ComputeEngine(object):
     NOP = 0  # in case we can simplify the instruction set
@@ -19,6 +21,10 @@ class ComputeEngine(object):
 
     OP_IDV_REG = 60
     OP_IDV_NUM = 61
+    OP_RT_UP_REG = 62
+    OP_RT_UP_NUM = 63
+    OP_RT_DN_REG = 63
+    OP_RT_DN_NUM = 64
 
     OP_JGZ_REG = 70
     OP_JGZ_NUM = 71
@@ -52,6 +58,11 @@ class ComputeEngine(object):
 
             ComputeEngine.OP_IDV_REG: self._op_idv_reg, 
             ComputeEngine.OP_IDV_NUM: self._op_idv_num,
+
+            ComputeEngine.OP_RT_UP_REG: self._op_rt_up_reg, 
+            ComputeEngine.OP_RT_UP_NUM: self._op_rt_up_num,
+            ComputeEngine.OP_RT_DN_REG: self._op_rt_dn_reg, 
+            ComputeEngine.OP_RT_DN_NUM: self._op_rt_dn_num,
 
             ComputeEngine.OP_JGZ_REG: self._op_jgz_reg,
             ComputeEngine.OP_JGZ_NUM: self._op_jgz_num,
@@ -125,10 +136,16 @@ class ComputeEngine(object):
         return (ComputeEngine.OP_JGZ_REG, check_val, to_val)
 
     def preprocess(self, cmds):
+        import math
         resolve = {
             'set': lambda cmd: ComputeEngine._parse_cmd_two_arity(ComputeEngine.OP_SET_NUM, ComputeEngine.OP_SET_REG, cmd),
             'sub': lambda cmd: ComputeEngine._parse_cmd_two_arity(ComputeEngine.OP_SUB_NUM, ComputeEngine.OP_SUB_REG, cmd),
             'mul': lambda cmd: ComputeEngine._parse_cmd_two_arity(ComputeEngine.OP_MUL_NUM, ComputeEngine.OP_MUL_REG, cmd),
+            'idv': lambda cmd: ComputeEngine._parse_cmd_two_arity(ComputeEngine.OP_IDV_NUM, ComputeEngine.OP_IDV_REG, cmd),
+            'mod': lambda cmd: ComputeEngine._parse_cmd_two_arity(ComputeEngine.OP_MOD_NUM, ComputeEngine.OP_MOD_REG, cmd),
+            '√up': lambda cmd: ComputeEngine._parse_cmd_two_arity(ComputeEngine.OP_RT_UP_NUM, ComputeEngine.OP_RT_UP_REG, cmd),
+            '√dn': lambda cmd: ComputeEngine._parse_cmd_two_arity(ComputeEngine.OP_RT_DN_NUM, ComputeEngine.OP_RT_DN_REG, cmd),
+
             'jnz': ComputeEngine._parse_jnz,
         }
 
@@ -138,7 +155,7 @@ class ComputeEngine(object):
             symbols.extend([sym for sym in inst[1:] if type(sym) == str])
         self.registers = { k: 0 for k in set(symbols) }
 
-    def process(self, on_op_result_func=lambda x: False, action_handler= lambda x: None):
+    def process_debug(self, on_op_result_func=lambda x: False, action_handler= lambda x, y, z: None):
         pc = 0
         cmd_len = len(self.instructions)
         while 0 <= pc < cmd_len:
@@ -147,13 +164,34 @@ class ComputeEngine(object):
             if action == None:
                 print(f"Unknown command: {cmd[0]}. Please check script (instruction on line {pc+1})")
                 break
-            action_handler(cmd[0])
+            action_handler(cmd[0], pc, self)
             result = action(*cmd[1:])
             result = 1 if result is None else result
             stop = on_op_result_func(result)
             if stop:
                 break
             pc += result
+
+    def process_optimized(self):
+        """ Streamlined version of process without bells and whisles (roughly 2x speed)"""
+        pc = 0
+        cmd_len = len(self.instructions)
+        # The below is a slight speed improvement, at the cost of lots of memory. 
+        op_list = [None] * (max(self.std_lib) + 1)
+        for k, v in self.std_lib.items():
+            op_list[k] = v
+
+        while 0 <= pc < cmd_len:
+            cmd = self.instructions[pc]
+            action = op_list[cmd[0]]
+            result = action(cmd[1], cmd[2])
+            pc += 1 if result is None else result
+
+    def get_register(self, which):
+        return self.registers[which]
+    
+    def _s_reg(self):
+        return sorted(self.registers.items())
 
     def _op_set_num(self, which, val): 
         self.registers[which] = val
@@ -188,6 +226,20 @@ class ComputeEngine(object):
     def _op_idv_num(self, which, val):
         self.registers[which] = self.registers[which] // val
 
+    
+    def _op_rt_up_num(self, which, val):
+        self.registers[which] = math.ceil(math.sqrt(val))
+
+    def _op_rt_up_reg(self, which, reg):
+        return self._op_rt_up_num(which, self.registers[reg])
+
+    def _op_rt_dn_num(self, which, val):
+        self.registers[which] = math.floor(math.sqrt(val))
+
+    def _op_rt_dn_reg(self, which, reg):
+        return self._op_rt_dn_num(which, self.registers[reg])
+
+
     def _op_idv_reg(self, which, reg):
         self._op_idv_num(which, self.registers[reg])
 
@@ -207,4 +259,4 @@ class ComputeEngine(object):
         return to if self.registers[reg] != 0 else 1
     
     def _op_jnz_reg(self, reg, reg_to):
-        return self._op_jgz_num(reg, self.registers[reg_to])
+        return self._op_jnz_num(reg, self.registers[reg_to])
